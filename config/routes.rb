477b4a204f20421f85ffe2e949dc35d52697ebfa
iqbal89co <iqbalfakhriza089@gmail.com>
2022-06-29 14:33:39 +0700

    add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   resources :company_data
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # auth token : eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE2NTY1NzQyMDZ9.DROtn0X1QYy6YiNfg-WE_x_8voBNdFPZ1JwzKTMeVww
+
+  resources :users, param: :_username
+  post '/auth/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
+
   # Defines the root path route ("/")
   # root "articles#index"
   get '/users/profile/:id', to: 'user_data#show'
